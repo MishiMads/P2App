007a0075fd9a3ea9920669180699d87fb50d9b54
Tallest boi of all (Markus) <mflens22@student.aau.dk>
2023-04-11 13:19:58 +0200

    Added P2App android studio

---
+++ P2App/app/src/main/java/com/example/p2plantapp/MainActivity.java
@@ -0,0 +1,35 @@
+package com.example.p2plantapp;
+
+import android.os.Bundle;
+
+import com.google.android.material.snackbar.Snackbar;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.view.View;
+
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
+
+import com.example.p2plantapp.databinding.ActivityMainBinding;
+
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class MainActivity extends AppCompatActivity {
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+
+    }
+
+
+
+
+}
---
+++ P2App/app/src/main/java/com/example/p2plantapp/SecondFragment.java
@@ -0,0 +1,35 @@
+package com.example.p2plantapp;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.navigation.fragment.NavHostFragment;
+
+import com.example.p2plantapp.databinding.FragmentSecondBinding;
+
+public class SecondFragment extends Fragment {
+
+
+
+    @Override
+    public View onCreateView(
+            LayoutInflater inflater, ViewGroup container,
+            Bundle savedInstanceState
+    ) {
+
+        return null;
+    }
+
+    public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+
+    }
+
+
+
+}

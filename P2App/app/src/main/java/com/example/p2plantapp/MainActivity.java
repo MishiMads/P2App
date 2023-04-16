76b2e4de0171889a9663306e31e40198b92931bf
Tallest boi of all (Markus) <mflens22@student.aau.dk>
2023-04-16 14:05:47 +0200

    Appen crasher ikke længere... det tog også kun 3 dage :D

--- P2App/app/src/main/java/com/example/p2plantapp/MainActivity.java
+++ P2App/app/src/main/java/com/example/p2plantapp/MainActivity.java
@@ -1,35 +1,50 @@
 package com.example.p2plantapp;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import com.google.android.material.snackbar.Snackbar;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.util.Log;
 import android.view.View;
 
-import androidx.navigation.NavController;
-import androidx.navigation.Navigation;
-import androidx.navigation.ui.AppBarConfiguration;
-import androidx.navigation.ui.NavigationUI;
+import androidx.fragment.app.FragmentTransaction;
 
-import com.example.p2plantapp.databinding.ActivityMainBinding;
+
 
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
 
+    private Button Next;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Next = (Button) findViewById(R.id.Next);
 
 
+        Next.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Openpage2();
+                Log.d("Buttons","To page 2!");
+                Toast.makeText(MainActivity.this, "Loading page 2",Toast.LENGTH_SHORT).show();
+
+            }
+        });
     }
-
-
-
+    public void Openpage2(){
+        Intent Open = new Intent(this, SecondActivity.class);
+        startActivity(Open);
+    }
 
 }
---
+++ P2App/app/src/main/java/com/example/p2plantapp/SecondActivity.java
@@ -0,0 +1,41 @@
+package com.example.p2plantapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class SecondActivity extends AppCompatActivity {
+
+    private Button prev;
+
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+        prev = (Button) findViewById(R.id.Prev);
+        prev.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Log.d("Buttons","To page 3!");
+                Openpage2();
+                Toast.makeText(SecondActivity.this, "Loading page 3",Toast.LENGTH_SHORT).show();
+
+
+            }
+        });
+    }
+
+
+    public void Openpage2(){
+        Intent Open = new Intent(this, MainActivity.class);
+        startActivity(Open);
+    }
+}

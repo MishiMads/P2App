613efa20b63a1adb0e43f6b5d0eb04e8ce0c510f
Tallest boi of all (Markus) <mflens22@student.aau.dk>
2023-04-18 11:08:42 +0200

    Fixed itempage

---
+++ P2App/app/src/main/java/com/example/p2plantapp/Itempage.java
@@ -0,0 +1,12 @@
+package com.example.p2plantapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Itempage extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_itempage);
+    }
+}

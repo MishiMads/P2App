3ffccfeacd06ad08377b029d6ffe9c71b4fa8176
Tallest boi of all (Markus) <mflens22@student.aau.dk>
2023-04-18 12:52:26 +0200

    Map on item page

--- P2App/app/src/main/java/com/example/p2plantapp/SecondActivity.java
+++ P2App/app/src/main/java/com/example/p2plantapp/SecondActivity.java
@@ -2,25 +2,31 @@ package com.example.p2plantapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.Toast;
 
 public class SecondActivity extends AppCompatActivity {
 
     private Button Front;
+    private ImageButton IP;
 
 
 
 
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
+
         Front = (Button) findViewById(R.id.FrontPage);
+
         Front.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -31,11 +37,22 @@ public class SecondActivity extends AppCompatActivity {
 
             }
         });
+        IP = (ImageButton) findViewById(R.id.Page3);
+        IP.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                OpenPage3();
+            }
+        });
     }
 
 
     public void Openpage2(){
         Intent Open = new Intent(this, MainActivity.class);
         startActivity(Open);
+    }
+    public void OpenPage3(){
+        Intent Open3 = new Intent(this, Itempage.class);
+        startActivity(Open3);
     }
 }

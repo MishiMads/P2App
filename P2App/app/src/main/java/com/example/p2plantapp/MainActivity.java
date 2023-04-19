8c7d331bf7d323742c8f49abb79a9c5539285968
Tallest boi of all (Markus) <mflens22@student.aau.dk>
2023-04-19 08:44:26 +0200

    Camera save photo

--- P2App/app/src/main/java/com/example/p2plantapp/MainActivity.java
+++ P2App/app/src/main/java/com/example/p2plantapp/MainActivity.java
@@ -1,23 +1,19 @@
 package com.example.p2plantapp;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
 import android.os.Bundle;
 
-import com.google.android.material.snackbar.Snackbar;
-
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.provider.MediaStore;
 import android.util.Log;
 import android.view.View;
 
-import androidx.fragment.app.FragmentTransaction;
 
-
-
-import android.view.Menu;
-import android.view.MenuItem;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
@@ -25,28 +21,45 @@ public class MainActivity extends AppCompatActivity {
 
     private Button Next;
     Button camera;
+    ImageView ImageDisplay;
+    public static final int CAMERA_ACTION_CODE =1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         camera = (Button)findViewById(R.id.Quiz);
+        ImageDisplay = findViewById(R.id.TestImage);
+
         camera.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                try{
+                Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                if (intent.resolveActivity(getPackageManager()) !=null){
+                    startActivityForResult(intent, CAMERA_ACTION_CODE);
+                }
+                else {
+                    Toast.makeText(MainActivity.this, "how the fuck do you not have a camera", Toast.LENGTH_SHORT).show();
+                }
+
+               /* try{
                     Intent intent = new Intent();
                     intent.setAction(MediaStore.ACTION_IMAGE_CAPTURE);
                     startActivity(intent);
+
                 }
                 catch(Exception e){
                     e.printStackTrace();
                 }
 
+                */
+
             }
         });
 
-        Next = (Button) findViewById(R.id.Next);
+
+
+       Next = (Button) findViewById(R.id.Next);
 
 
         Next.setOnClickListener(new View.OnClickListener() {
@@ -62,6 +75,15 @@ public class MainActivity extends AppCompatActivity {
     public void Openpage2(){
         Intent Open = new Intent(this, SecondActivity.class);
         startActivity(Open);
+    }
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == CAMERA_ACTION_CODE && resultCode == RESULT_OK && data != null){
+            Bundle bundle = data.getExtras();
+            Bitmap finalPhoto = (Bitmap) bundle.get("data");
+            ImageDisplay.setImageBitmap(finalPhoto);
+        }
     }
 
 }

80fc9bd54636654c7613dd26efab13afa1ee5271
Tallest boi of all (Markus) <mflens22@student.aau.dk>
2023-04-19 09:55:00 +0200

    Camera push2

--- P2App/app/src/main/java/com/example/p2plantapp/SecondActivity.java
+++ P2App/app/src/main/java/com/example/p2plantapp/SecondActivity.java
@@ -19,7 +19,6 @@ public class SecondActivity extends AppCompatActivity {
 
 
 
-    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);

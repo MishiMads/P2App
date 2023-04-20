2ae3fd936136ff6f65ca2bf080ee5095f799de91
Gucci HO <tho22@student.aau.dk>
2023-04-20 11:32:35 +0200

    Added comments to the Birds.java file

--- P2App/app/src/main/java/com/example/p2plantapp/Birds.java
+++ P2App/app/src/main/java/com/example/p2plantapp/Birds.java
@@ -2,15 +2,30 @@ package com.example.p2plantapp;
 import android.graphics.Bitmap;
 import java.util.HashMap;
 
-public class Birds {
-    private static HashMap<String, Bitmap> birdMap = new HashMap<>();
-
+public class Birds { //Hashmap for the collection of pictures including their user-inputted names.
+    private static HashMap<String, Bitmap> birdMap = new HashMap<>(); //A hashmap is created
+    //with the name birdMap with String as its key and Bitmap as its value.
+    //The birdMap Hashmap is a private static, meaning it can only be accessed
+    //by the methods of this specific class and no other classes.
+        //Private: only methods of the same class can access this member.
+        //Static: only one instance of this hashmap exists across all instances of the Birds class.
+            //I.e multiple instances of the Birds class share the same (this) hashmap named birdMap.
     public static void addBird(String key, Bitmap bitmap) {
         birdMap.put(key, bitmap);
-    }
+    } //A method
+    //called "addBird" which takes two arguments: a String and Bitmap and assigned them to
+    //"key" and "bitmap" respectively. In the code block, the .put() method in the hashmap class
+    //is used to take the values from the method arguments
+    //and add them as an element in the birdMap hashmap.
+    //This method is public and static with a return type of void:
+        //Public: accessible from anywhere in the program.
+            //They can be accessed by objects of the class, as well as objects of other classes
+        //Static: method belongs to the class itself rather than instances of the class.
+            //Also means the method can be called without creating an instance of the class.
+        //Void: the method does not return anything after being executed.
 
     public static Bitmap getBird(String key) {
         return birdMap.get(key);
-    }
+    } //WIP...
 
 }

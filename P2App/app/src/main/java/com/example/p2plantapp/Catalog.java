fe2410f3456fd86951a6930d27dd20e89ce7ac09
Tallest boi of all (Markus) <mflens22@student.aau.dk>
2023-04-24 13:24:02 +0200

    catalog changes

--- P2App/app/src/main/java/com/example/p2plantapp/Catalog.java
+++ P2App/app/src/main/java/com/example/p2plantapp/Catalog.java
@@ -66,7 +66,7 @@ public class Catalog extends AppCompatActivity {
             catalogLayout.addView(imageView);
             Log.d("myTag", "Forloop ended");
         }
-        
+
         Button takePictureButton = findViewById(R.id.PicBut);
         takePictureButton.setOnClickListener(new View.OnClickListener() {
 
